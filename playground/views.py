c6a946fc2a3d95ca8da1a79efaff617b7a0d7955
Brayden Carlson <braydenc@stratusadv.com>
2024-10-31 16:28:07 -0600

    Show the activity in the template

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.template.response import TemplateResponse
 from playground.utils import generate_test_model
 
@@ -12,13 +13,31 @@ def home_page_view(request):
 def test_model_view(request):
     model = generate_test_model()
 
+    user, created = User.objects.get_or_create(
+        username="test_user",
+        defaults={"password": "test_password"}
+    )
+
     fields = {
         field.name: getattr(model, field.name)
         for field in model._meta.fields
     }
 
+    model.add_activity(
+        user=user,
+        verb='created',
+        information=f'{request.user} added a model.'
+    )
+
+    activities = model.activity_log.all()
+
+    context = {
+        'activities': activities,
+        'fields': fields
+    }
+
     return TemplateResponse(
         request,
-        context={'fields': fields},
+        context=context,
         template='page/test_model.html'
     )
