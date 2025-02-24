c6459039efed77f1d03053d28d7e04930d6f2cc1
Ralph Uy <ralphu@stratusadv.com>
2025-02-24 16:15:52 -0700

    Add model_field_prompt function to create a generic field prompting that requires mandatory choices

---
+++ django_spire/seeding/intelligence/prompts/model_field_choices_prompt.py
@@ -0,0 +1,19 @@
+from django.db.models import Model
+
+from typing import Type, List, Text
+
+from dandy.llm import Prompt
+
+
+def model_field_choices_prompt(
+        model_field: str,
+        choices_list: List[str],
+) -> Prompt:
+    model_name = model_field.field.model._meta.verbose_name
+    field_name = model_field.field.attname
+    return (
+        Prompt()
+        .heading(f'Generate values for {field_name} in {model_name} model')
+        .text(f'The value for the {field_name} field should only be selected from the following list of choices.')
+        .list(choices_list)
+    )
