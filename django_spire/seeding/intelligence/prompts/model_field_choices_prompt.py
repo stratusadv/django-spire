bcd9beaab6575df13d57c71c8a4caf8941640796
Ralph Uy <ralphu@stratusadv.com>
2025-02-25 11:22:50 -0700

    Refactor variable names. Change attribute typing

--- django_spire/seeding/intelligence/prompts/model_field_choices_prompt.py
+++ django_spire/seeding/intelligence/prompts/model_field_choices_prompt.py
@@ -1,16 +1,17 @@
-from typing import List
-
 from dandy.llm import Prompt
+from django.db.models.enums import TextChoices
+from django.db.models.fields import Field
+
 
 def model_field_choices_prompt(
-        model_field: str,
-        choices_list: List[str],
+        model_field: Field,
+        choices: TextChoices,
 ) -> Prompt:
-    model_name = model_field.field.model._meta.verbose_name
-    field_name = model_field.field.attname
+    model_name = model_field.field.model.__name__
+    field_name = model_field.field.name
     return (
         Prompt()
         .heading(f'Generate values for {field_name} in {model_name} model')
         .text(f'The value for the {field_name} field should only be selected from the following list of choices.')
-        .list(choices_list)
+        .list([choice[0] for choice in choices])
     )
