4e4aad28f37d9cdd8e323f66fac70992edfd3fc8
Austin Sauer <austins@stratusadv.com>
2025-03-04 11:09:45 -0700

    Cleaned up admin files

--- django_spire/history/admin.py
+++ django_spire/history/admin.py
@@ -3,54 +3,6 @@ from django.urls import reverse
 from django.utils.html import format_html
 
 from django_spire.history import models
-from django_spire.history.activity.models import ActivityLog
-from django_spire.history.view.models import Viewed
-
-
-
-@admin.register(ActivityLog)
-class ActivityLogAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'content_object_link', 'content_type', 'created_datetime',
-        'verb', 'user_link', 'recipient_link', 'information_snippet'
-    )
-    list_filter = ('verb', 'created_datetime')
-    search_fields = ('id', 'user__first_name', 'user__last_name', 'recipient__first_name', 'recipient__last_name', 'content_type__model', 'verb')
-    ordering = ('-created_datetime',)
-
-    def content_object_link(self, activity: ActivityLog) -> str:
-        url = reverse(
-            f'admin:{activity.content_type.app_label}_{activity.content_type.model}_change',
-            args=[activity.object_id]
-        )
-
-        return format_html(f'<a href="{url}">{activity.content_object}</a>')
-
-    content_object_link.short_description = 'Content Object'
-
-    def user_link(self, activity: ActivityLog) -> str:
-        url = reverse('admin:auth_user_change', args=[activity.user.id])
-        return format_html(f'<a href="{url}">{activity.user.get_full_name()}</a>')
-
-    user_link.short_description = 'User'
-
-    def recipient_link(self, activity: ActivityLog) -> str:
-        if activity.recipient:
-            url = reverse('admin:auth_user_change', args=[activity.recipient.id])
-            return format_html(f'<a href="{url}">{activity.recipient.get_full_name()}</a>')
-
-        return 'No Recipient'
-
-    recipient_link.short_description = 'Recipient'
-
-    def information_snippet(self, activity: ActivityLog) -> str:
-        return (
-            activity.information[:20] + '...'
-            if activity.information and len(activity.information) > 20
-            else activity.information or 'No Information'
-        )
-
-    information_snippet.short_description = 'Information Snippet'
 
 
 @admin.register(models.EventHistory)
@@ -71,29 +23,3 @@ class EventHistoryAdmin(admin.ModelAdmin):
         return format_html(f'<a href="{url}">{event_history.content_object}</a>')
 
     content_object_link.short_description = 'Content Object'
-
-
-@admin.register(Viewed)
-class ViewAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'content_object_link', 'user_link', 'created_datetime'
-    )
-    list_filter = ('created_datetime',)
-    search_fields = ('id', 'user__first_name', 'user__last_name', 'content_type__model',)
-    ordering = ('-created_datetime',)
-
-    def content_object_link(self, view: Viewed) -> str:
-        url = reverse(
-            f'admin:{view.content_type.app_label}_{view.content_type.model}_change',
-            args=[view.object_id]
-        )
-
-        return format_html(f'<a href="{url}">{view.content_object}</a>')
-
-    content_object_link.short_description = 'Content Object'
-
-    def user_link(self, view: Viewed) -> str:
-        url = reverse('admin:auth_user_change', args=[view.user.id])
-        return format_html(f'<a href="{url}">{view.user.get_full_name()}</a>')
-
-    user_link.short_description = 'User'
--- django_spire/history/view/admin.py
+++ django_spire/history/view/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.urls.base import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from django_spire.history.view.models import Viewed
+
+
+@admin.register(Viewed)
+class ViewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'content_object_link', 'user_link', 'created_datetime'
+    )
+    list_filter = ('created_datetime',)
+    search_fields = ('id', 'user__first_name', 'user__last_name', 'content_type__model',)
+    ordering = ('-created_datetime',)
+
+    def content_object_link(self, view: Viewed) -> str:
+        url = reverse(
+            f'admin:{view.content_type.app_label}_{view.content_type.model}_change',
+            args=[view.object_id]
+        )
+
+        return format_html(f'<a href="{url}">{view.content_object}</a>')
+
+    content_object_link.short_description = 'Content Object'
+
+    def user_link(self, view: Viewed) -> str:
+        url = reverse('admin:auth_user_change', args=[view.user.id])
+        return format_html(f'<a href="{url}">{view.user.get_full_name()}</a>')
+
+    user_link.short_description = 'User'
