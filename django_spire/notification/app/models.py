79945aabf6394f6f1842404d9e7a002bd87e38cf
Austin Sauer <austins@stratusadv.com>
2025-03-07 09:46:46 -0700

    Generic relationship on AppNotification model

--- django_spire/notification/app/models.py
+++ django_spire/notification/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 from django_spire.history.viewed.mixins import ViewedModelMixin
 from django_spire.notification.models import Notification
@@ -7,6 +8,8 @@ from django_spire.notification.models import Notification
 class AppNotification(ViewedModelMixin):
     notification = models.ForeignKey(Notification, editable=False, on_delete=models.CASCADE)
     user = models.ForeignKey(User, editable=False, on_delete=models.CASCADE)
+    content_object = GenericForeignKey('content_type', 'object_id')
+    object_id = models.PositiveIntegerField()
 
     def __str__(self):
         return f'{self.notification.title} - {self.email}'
