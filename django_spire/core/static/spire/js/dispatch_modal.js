1a849643a259beca67bef7d4d6c011afc5d6cebd
Wesley Howery <wesleyh@stratusadv.com>
2025-03-26 15:25:49 -0600

    glue js refactor 0.8.1

--- django_spire/core/static/spire/js/dispatch_modal.js
+++ django_spire/core/static/spire/js/dispatch_modal.js
@@ -1,5 +1,5 @@
 async function dispatch_modal_view(url, payload = {}) {
-    let view = new GlueView(url);
+    let view = new ViewGlue(url);
     let html = await view.get(payload);
     dispatch_modal(html);
 }
