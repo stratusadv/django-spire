ab9bf91dd5df3ed80951f92fc2436c3b51605752
TyrellMartens <tyrellm@stratusadv.com>
2025-04-29 11:43:24 -0600

    Implemented deletion of AI Chats.

--- django_spire/ai/chat/urls/template_urls.py
+++ django_spire/ai/chat/urls/template_urls.py
@@ -6,7 +6,8 @@ from django_spire.ai.chat.views import template_views
 app_name = 'template'
 
 urlpatterns = [
-    path('load/', template_views.load_chat_view, name='load'),
-    path('recent/', template_views.recent_chat_list_view, name='recent'),
-    path('search/', template_views.search_chat_view, name='search'),
+    path("<int:pk>/confirm_delete", template_views.confirm_delete_view, name="confirm_delete"),
+    path("load/", template_views.load_chat_view, name="load"),
+    path("recent/", template_views.recent_chat_list_view, name="recent"),
+    path("search/", template_views.search_chat_view, name="search"),
 ]
