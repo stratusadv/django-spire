3c78630908ebfe07cd2de3370407950ea10dcdfd
O'Brien Little <obrienl@stratusadv.com>
2025-05-09 16:34:06 -0600

    Removed ability to delete notifications

--- django_spire/notification/app/urls/template_urls.py
+++ django_spire/notification/app/urls/template_urls.py
@@ -8,8 +8,5 @@ app_name = 'django_spire_notification'
 urlpatterns = [
     path('notficiation/dropdown/template/',
          template_views.notification_dropdown_template_view,
-         name='notification_dropdown'),
-    path('<int:pk>/notification/delete/template/',
-         template_views.notification_delete_template_view,
-         name='delete_notification')
+         name='notification_dropdown')
 ]
