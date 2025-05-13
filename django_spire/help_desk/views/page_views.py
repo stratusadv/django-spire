f50df1725f9d0fff79a0653be297100535706cd0
chasemstratusadv <chasem@stratusadv.com>
2025-05-13 14:43:46 -0600

    switch template response to portal_views.list_view response in the ticket list view

--- django_spire/help_desk/views/page_views.py
+++ django_spire/help_desk/views/page_views.py
@@ -2,15 +2,30 @@ from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 
 from django_spire.help_desk.models import HelpDeskTicket
+from django_spire.contrib.generic_views import portal_views
 
-@login_required
+@login_required()
 def ticket_list_view(request):
     tickets = HelpDeskTicket.objects.order_by('-created_datetime').all()
+
+    return portal_views.list_view(
+        request=request,
+        context_data={
+            "tickets": tickets
+        },
+        model=HelpDeskTicket,
+        template='django_spire/help_desk/page/ticket_list_page.html'
+    )
+
+
+@login_required()
+def ticket_detail_view(request, pk):
+    ticket = HelpDeskTicket.objects.get(pk=pk)
 
     return TemplateResponse(
         request,
         context = {
-            "tickets": tickets
+            "ticket": ticket
         },
-        template = 'django_spire/help_desk/page/ticket_list_page.html'
+        template = 'django_spire/help_desk/page/ticket_detail_page.html'
     )
