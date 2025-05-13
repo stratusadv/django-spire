15f79b4813e461e8e5f38dcffaf891cdd1370af3
chasemstratusadv <chasem@stratusadv.com>
2025-05-13 14:10:54 -0600

    add missing login_required decorator to help desk list_view

--- django_spire/help_desk/views/page_views.py
+++ django_spire/help_desk/views/page_views.py
@@ -1,8 +1,9 @@
 from django.template.response import TemplateResponse
+from django.contrib.auth.decorators import login_required
 
 from django_spire.help_desk.models import HelpDeskTicket
 
-
+@login_required
 def ticket_list_view(request):
     tickets = HelpDeskTicket.objects.order_by('-created_datetime').all()
 
