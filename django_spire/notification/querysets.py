f786aa858549aeb889d1395916975fe6d56c89f4
O'Brien Little <obrienl@stratusadv.com>
2025-05-15 15:34:07 -0600

    Added querysets to help notifications

--- django_spire/notification/querysets.py
+++ django_spire/notification/querysets.py
@@ -1,13 +1,20 @@
+from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 from django_spire.history.querysets import HistoryQuerySet
-from django_spire.notification.choices import NotificationStatusChoices, \
-    NotificationTypeChoices
+from django_spire.notification.choices import (
+    NotificationStatusChoices,
+    NotificationTypeChoices,
+    NotificationPriorityChoices,
+)
 
 
 class NotificationQuerySet(HistoryQuerySet):
     def app_notifications(self):
         return self.filter(type=NotificationTypeChoices.APP)
+
+    def by_user(self, user: User):
+        return self.filter(user=user)
 
     def email_notifications(self):
         return self.filter(type=NotificationTypeChoices.EMAIL)
@@ -15,14 +22,32 @@ class NotificationQuerySet(HistoryQuerySet):
     def errored(self):
         return self.filter(status=NotificationStatusChoices.ERRORED)
 
+    def failed(self):
+        return self.filter(status=NotificationStatusChoices.FAILED)
+
+    def high_priority(self):
+        return self.filter(priority=NotificationPriorityChoices.HIGH)
+
+    def low_priority(self):
+        return self.filter(priority=NotificationPriorityChoices.LOW)
+
+    def medium_priority(self):
+        return self.filter(priority=NotificationPriorityChoices.MEDIUM)
+
     def pending(self):
         return self.filter(status=NotificationStatusChoices.PENDING)
+
+    def processing(self):
+        return self.filter(status=NotificationStatusChoices.PROCESSING)
 
     def push_notifications(self):
         return self.filter(status=NotificationTypeChoices.PUSH)
 
     def ready_to_send(self):
         return self.pending().filter(publish_datetime__lte=now())
+
+    def sent(self):
+        return self.filter(status=NotificationStatusChoices.SENT)
 
     def sms_notifications(self):
         return self.filter(type=NotificationTypeChoices.SMS)
