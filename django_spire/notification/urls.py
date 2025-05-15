ce21e61f3c1759820a687fec3ff4abb09ef49eea
TyrellMartens <tyrellm@stratusadv.com>
2025-05-15 09:42:56 -0600

    Mixin and todo.

--- django_spire/notification/urls.py
+++ django_spire/notification/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 app_name = 'notification'
 
 urlpatterns = [
+    # TODO: Change to app
     path('app/', include('django_spire.notification.app.urls', namespace='group')),
 ]
