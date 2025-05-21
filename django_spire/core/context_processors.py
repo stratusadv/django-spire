d1590d1ffe63958b716c6cad7d7aefa7ea99d1b4
chasemstratusadv <chasem@stratusadv.com>
2025-05-21 16:53:07 -0600

    get help desk icon from context processor, fix badge layout in ticket_item, change ticket_form_card title to switch based on ticket data instead of path

--- django_spire/core/context_processors.py
+++ django_spire/core/context_processors.py
@@ -3,18 +3,15 @@ from __future__ import annotations
 from typing_extensions import TYPE_CHECKING, Any
 
 from django_spire.consts import __VERSION__
+from django.urls import reverse
 
 if TYPE_CHECKING:
     from django.core.handlers.wsgi import WSGIRequest
-
 
 def django_spire(_: WSGIRequest) -> dict[str, Any]:
     return {
         'DJANGO_SPIRE_VERSION': __VERSION__,
         'app_bootstrap_icon': {
-            'ai': 'ai_icon_string',
-            'ai_chat': 'chat_icon_string',
-            'help_desk': 'help_desk_icon_string',
-            'other': 'something'
+            'help_desk': 'bi bi-headset'
         }
     }
--- test_project/apps/core/context_processors.py
+++ test_project/apps/core/context_processors.py
@@ -3,17 +3,27 @@ from __future__ import annotations
 from django.core.handlers.wsgi import WSGIRequest
 from django.urls import reverse
 from typing_extensions import Any
-from django_spire.core.context_processors import django_spire as django_spire_context_processor
+from django_spire.core.context_processors import django_spire as django_spire_default
 
 
-def test_project(_: WSGIRequest) -> dict[str, list[dict[str, str]]]:
+# Demonstrating how to override the default django spire context processor data
+def django_spire(request: WSGIRequest) -> dict[str, Any]:
+    context_data = django_spire_default(request)
+    context_data['app_bootstrap_icon']['help_desk'] = 'bi bi-headset'
+
+    return context_data
+
+
+def test_project(request: WSGIRequest) -> dict[str, list[dict[str, str]]]:
+    spire_context_data = django_spire(request)
+
     return {
         'test_project_apps': [
             {'heading': 'Django Spire'},
             {'title': 'Ai Management', 'icon': 'bi bi-robot', 'url': reverse('ai:home')},
             {'title': 'Ai Chat', 'icon': 'bi bi-chat-heart', 'url': reverse('ai:chat:home')},
             {'title': 'Comment', 'icon': 'bi bi-chat-text', 'url': reverse('comment:home')},
-            {'title': 'Help Desk', 'icon': 'bi bi-headset', 'url': reverse('help_desk:home')},
+            {'title': 'Help Desk', 'icon': spire_context_data['app_bootstrap_icon']['help_desk'], 'url': reverse('help_desk:home')},
             {'title': 'History', 'icon': 'bi bi-clock-history', 'url': reverse('history:home')},
             {'title': 'Home', 'icon': 'bi bi-house-door', 'url': reverse('home:page:home')},
             {'title': 'Notification', 'icon': 'bi bi-bell', 'url': reverse('notification:page:home')},
@@ -23,10 +33,3 @@ def test_project(_: WSGIRequest) -> dict[str, list[dict[str, str]]]:
             {'title': 'Tabular', 'icon': 'bi bi-table', 'url': reverse('tabular:home')},
         ]
     }
-
-
-def django_spire(request: WSGIRequest) -> dict[str, Any]:
-    context_data = django_spire_context_processor(request)
-    context_data['app_bootstrap_icon']['other'] = 'testy_test_icon_string'
-
-    return context_data
