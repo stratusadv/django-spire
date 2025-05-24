9f5bea6269cc159152ea1d9e6c014911cdc0d460
Wesley Howery <wesleyh@stratusadv.com>
2025-05-24 09:08:52 -0600

    add update_model_fields service

---
+++ django_spire/contrib/services/tests/test_update_service.py
@@ -0,0 +1,62 @@
+from django.contrib.auth.models import Group
+from django.core.exceptions import FieldDoesNotExist
+from django.test import TestCase
+
+from django_spire.auth.group.models import AuthGroup
+from django_spire.auth.mfa.models import MfaCode
+from django_spire.auth.user.tests.factories import create_super_user
+from django_spire.contrib.services.update_service import update_model_fields
+
+
+class UpdateServiceTestCase(TestCase):
+    def setUp(self):
+        self.user = create_super_user()
+        self.group = Group.objects.create(name='Bobberts Minions')
+
+    def test_valid_update_model_fields(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Smith'
+        }
+
+        user = update_model_fields(self.user, **data)
+        self.assertEqual(user.first_name, 'John')
+        self.assertEqual(user.last_name, 'Smith')
+
+    def test_invalid_field_name(self):
+        data = {
+            'invalid_field': 'test'
+        }
+
+        with self.assertRaises(FieldDoesNotExist):
+            update_model_fields(self.user, **data)
+
+    def test_fk_id_aliases(self):
+        mfa = MfaCode.objects.create(
+            user=self.user,
+            code='123456',
+            expiration_datetime='2023-01-01'
+        )
+        new_user = create_super_user()
+
+        data = {
+            'user_id': new_user.id,
+
+        }
+        mfa = update_model_fields(mfa, **data)
+        self.assertEqual(mfa.user_id, new_user.id)
+
+    def test_obj_fk_id_aliases(self):
+        mfa = MfaCode.objects.create(
+            user=self.user,
+            code='123456',
+            expiration_datetime='2023-01-01'
+        )
+        new_user = create_super_user()
+
+        data = {
+            'user': new_user,
+
+        }
+        mfa = update_model_fields(mfa, **data)
+        self.assertEqual(mfa.user_id, new_user.id)
---
+++ django_spire/contrib/services/update_service.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import Any
+
+from django.core.exceptions import FieldDoesNotExist, ValidationError
+from django.db import transaction
+from django.db.models import Model
+
+
+class InvalidFieldError(FieldDoesNotExist):
+    """Raised when a kwarg does not match a concrete updatable model field."""
+
+
+@transaction.atomic
+def update_model_fields(instance: Model, **data: Any) -> Model:
+    """
+    Apply keyword data to a model instance and persist it.
+    - Raises InvalidFieldError if a kwarg does not correspond to a concrete
+      (non-m2m, non-reverse) field on the model.
+    """
+    if not data:
+        return instance
+
+    concrete_fields = {
+        f.name for f in instance._meta.get_fields()
+        if f.concrete and not f.many_to_many and not f.one_to_many
+    }
+
+    # Allow *_id shortcut for FKs if desired
+    fk_id_aliases = {
+        f"{f.name}_id" for f in instance._meta.get_fields() if f.many_to_one
+    }
+    allowed_fields = concrete_fields | fk_id_aliases
+
+    for field, value in data.items():
+
+        if field not in allowed_fields:
+            raise InvalidFieldError(
+                f"'{field}' is not a valid updatable field on "
+                f"model '{instance.__class__.__name__}'
+            )
+
+        setattr(instance, field, value)
+
+    instance.full_clean(exclude=[f for f in concrete_fields if f not in data])
+    instance.save(
+        update_fields=[f for f in data.keys() if not f.endswith("_id")]
+    )
+    return instance
