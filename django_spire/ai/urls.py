3f80539be74a6324ec57f081bb0311743fa06d76
Nathan Johnson <nathanj@stratusadv.com>
2025-06-03 12:07:12 -0600

    fix urls for missing sms app

--- django_spire/ai/urls.py
+++ django_spire/ai/urls.py
@@ -4,5 +4,4 @@ app_name = 'ai'
 
 urlpatterns = [
     path('chat/', include('django_spire.ai.chat.urls', namespace='chat')),
-    path('sms/', include('django_spire.ai.sms.urls', namespace='sms')),
 ]
