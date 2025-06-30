01615d57a698ce6458a169f1fd65101a4a39a44b
O'Brien Little <obrienl@stratusadv.com>
2025-06-30 11:11:05 -0600

    TEsting compression values for sms temproary media

--- django_spire/notification/sms/views/media_views.py
+++ django_spire/notification/sms/views/media_views.py
@@ -1,5 +1,8 @@
 import base64
 import uuid
+from io import BytesIO
+
+from PIL import Image
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,7 +14,16 @@ from django_spire.notification.sms.models import SmsTemporaryMedia
 def external_temporary_media_view(request, external_access_key: uuid.UUID) -> HttpResponse:
     temporary_media = SmsTemporaryMedia.objects.get(external_access_key=external_access_key)
 
+    image = Image.open(
+        BytesIO(base64.b64decode(temporary_media.content))
+    ).convert('P', palette=Image.ADAPTIVE, colors=32)
+
+    image = image.resize((int(image.size[0]/2), int(image.size[1]/2)), resample=Image.Resampling.BICUBIC)
+
+    buffer = BytesIO()
+    image.save(buffer, 'PNG')
+
     return HttpResponse(
-        content=base64.b64decode(temporary_media.content),
+        content=buffer.getvalue(),
         content_type=temporary_media.content_type
     )
