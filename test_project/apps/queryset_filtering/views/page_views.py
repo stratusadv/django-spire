f91b9ccf6ddfd1d913d976c996482d20378556fb
Wesley Howery <wesleyh@stratusadv.com>
2025-07-04 08:04:46 -0600

    sudo code for thoughts

--- test_project/apps/queryset_filtering/views/page_views.py
+++ test_project/apps/queryset_filtering/views/page_views.py
@@ -1,15 +1,26 @@
+from django.contrib.messages.context_processors import messages
 from django.template.response import TemplateResponse
 
 from test_project.apps.queryset_filtering.models import Task
 
 
 def list_page(request):
+    # We have user input so a form should be used to validate the data.
+    filter_form = TaskListFilterForm(request.GET)
+
+    if not filter_form.is_valid():
+        # Send error message back to client
+        pass
+
+
+    session = QuerySetFilterSession(request, 'task_list_page') # THis can happen inside process session filter.
+
 
     tasks = (
         Task
         .objects
         .active()
-        .process_session_filter(request, 'task_list_page')
+        .process_session_filter(request, 'task_list_page', filter_form.cleaned_data) # How do I get the cleaned data or validate the form here?
         .search(request.GET.get('search_value'))
     )
 
