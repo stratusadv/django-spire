2fcd16627725207df7a1e39b109566063714920f
Wesley Howery <wesleyh@stratusadv.com>
2025-07-12 16:52:14 -0600

    extra filters to map tool

--- django_spire/contrib/queryset/filter_tools.py
+++ django_spire/contrib/queryset/filter_tools.py
@@ -1,22 +1,36 @@
+from __future__ import annotations
+
 from django.db.models import QuerySet
 
 
-def filter_by_lookup_map(queryset: QuerySet, lookup_map: dict, data: dict):
+def filter_by_lookup_map(
+        queryset: QuerySet,
+        lookup_map: dict,
+        data: dict,
+        extra_filters: dict | None = None
+):
     """
-    Filter a queryset using a lookup map that translates input keys to queryset lookup expressions.
+    Filters a given queryset based on a lookup map and provided data. Additional filters
+    can also be applied if provided.
 
-    Args:
-        queryset: The Django queryset to filter
-        lookup_map: A dictionary mapping input keys to queryset lookup expressions 
-        data: Dictionary of key-value pairs to filter on
+    Parameters:
+        queryset (QuerySet): The queryset to be filtered.
+        lookup_map (dict): A dictionary mapping input data keys to filtering fields in
+            the queryset.
+        data (dict): A dictionary containing key-value pairs to filter the queryset.
+        extra_filters (list | None): Optional. A list of extra positional filter
+            arguments to be applied. Defaults to None.
 
     Returns:
-        The filtered queryset based on the lookup map and data
+        QuerySet: The filtered queryset.
     """
+    if extra_filters is None:
+        extra_filters = {}
+
     lookup_kwargs = {
         lookup_map[k]: v
         for k, v in data.items()
         if k in lookup_map and v not in (None, "", [])
-    }
+    } | extra_filters
 
     return queryset.filter(**lookup_kwargs)
