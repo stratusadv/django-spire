33cad7345056d2cd7cd11f7fd31fce20e728937a
Wesley Howery <wesleyh@stratusadv.com>
2025-07-12 17:40:51 -0600

    session controller js calls with template tags

--- test_project/base_settings.py
+++ test_project/base_settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS += [
     'django_spire.contrib.breadcrumb',
     'django_spire.comment',
     'django_spire.core',
+    'django_spire.contrib.session',
     'django_spire.file',
     'django_spire.contrib.form',
     'django_spire.contrib.gamification',
