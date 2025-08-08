722510d4f496954d9030524747de46f7965c0349
Wesley Howery <wesleyh@stratusadv.com>
2025-08-08 10:13:14 -0600

    add pages for theme components (colors, typography, buttons, badges, borders) and update dashboard layout

--- test_project/apps/theme/urls/page_urls.py
+++ test_project/apps/theme/urls/page_urls.py
@@ -5,4 +5,9 @@ from test_project.apps.theme.views import page_views
 app_name = 'page'
 urlpatterns = [
     path('dashboard/', page_views.dashboard_view, name='dashboard'),
+    path('colors/', page_views.colors_view, name='colors'),
+    path('typography/', page_views.typography_view, name='typography'),
+    path('buttons/', page_views.buttons_view, name='buttons'),
+    path('badges/', page_views.badges_view, name='badges'),
+    path('borders/', page_views.borders_view, name='borders'),
 ]
