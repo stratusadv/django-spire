2d149cf6b94691faaa369349364bec21af5618bc
Tyrell Martens <tyrellm@stratusadv.com>
2025-08-28 19:37:12 -0600

    ### Changes - Added Django Glue Fields and Example Theme Page. - Changed hard-coded colours across system. - Added hover colouring on all buttons.
    ### Breaking
    - Potential broken CSS / colours. Review theme page for any issues.

--- django_spire/theme/views/page_views.py
+++ django_spire/theme/views/page_views.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from django.urls import reverse
 from typing_extensions import TYPE_CHECKING
 
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.template.response import TemplateResponse
 
 from django_spire.contrib import Breadcrumbs
@@ -14,6 +14,7 @@ if TYPE_CHECKING:
     from django.core.handlers.wsgi import WSGIRequest
 
 
+@login_required()
 def dashboard_view(request: WSGIRequest) -> TemplateResponse:
     crumbs = Breadcrumbs()
     crumbs.add_breadcrumb('Dashboard')
@@ -27,6 +28,7 @@ def dashboard_view(request: WSGIRequest) -> TemplateResponse:
     )
 
 
+@login_required()
 def colors_view(request: WSGIRequest) -> TemplateResponse:
     crumbs = Breadcrumbs()
     crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
@@ -41,6 +43,37 @@ def colors_view(request: WSGIRequest) -> TemplateResponse:
     )
 
 
+@login_required()
+def django_glue_view(request: WSGIRequest) -> TemplateResponse:
+    crumbs = Breadcrumbs()
+    crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
+    crumbs.add_breadcrumb('Django Glue')
+
+    return portal_views.template_view(
+        request,
+        page_title='Theme',
+        page_description='Django Glue',
+        breadcrumbs=crumbs,
+        template='django_spire/theme/page/django_glue_page.html'
+    )
+
+
+@login_required()
+def example_view(request: WSGIRequest) -> TemplateResponse:
+    crumbs = Breadcrumbs()
+    crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
+    crumbs.add_breadcrumb('Core Templates')
+
+    return portal_views.template_view(
+        request,
+        page_title='Theme',
+        page_description='Core Templates',
+        breadcrumbs=crumbs,
+        template='django_spire/theme/example/page/example_page.html'
+    )
+
+
+@login_required()
 def typography_view(request: WSGIRequest) -> TemplateResponse:
     crumbs = Breadcrumbs()
     crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
@@ -55,6 +88,7 @@ def typography_view(request: WSGIRequest) -> TemplateResponse:
     )
 
 
+@login_required()
 def buttons_view(request: WSGIRequest) -> TemplateResponse:
     crumbs = Breadcrumbs()
     crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
@@ -69,6 +103,7 @@ def buttons_view(request: WSGIRequest) -> TemplateResponse:
     )
 
 
+@login_required()
 def badges_view(request: WSGIRequest) -> TemplateResponse:
     crumbs = Breadcrumbs()
     crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
@@ -83,6 +118,7 @@ def badges_view(request: WSGIRequest) -> TemplateResponse:
     )
 
 
+@login_required()
 def borders_view(request: WSGIRequest) -> TemplateResponse:
     crumbs = Breadcrumbs()
     crumbs.add_breadcrumb('Dashboard', reverse('django_spire:theme:page:dashboard'))
