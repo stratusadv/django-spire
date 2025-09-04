31b7eef4e4cf359d16ea458ed907368b973c9310
Ralph Uy <ralphu@stratusadv.com>
2025-09-04 08:37:32 -0600

    Add different way of prompting AI

--- django_spire/auth/seeding/seeder.py
+++ django_spire/auth/seeding/seeder.py
@@ -4,20 +4,19 @@ from django_spire.contrib.seeding import DjangoModelSeeder
 
 
 class UserSeeder(DjangoModelSeeder):
-    model_class=User
+    model_class = User
     cache_seed = False
-
-    fields={
+    default_to = 'faker'
+    fields = {
         'id': 'exclude',
-        'username': ('faker', 'name'),
+        'username': ('faker', 'user_name'),
         'first_name': ('faker', 'first_name'),
         'last_name': ('faker', 'last_name'),
         'email': ('faker', 'email'),
         'is_staff': ('faker', 'boolean'),
-        'is_superuser': False,
+        'is_superuser': ('faker', 'boolean'),
         'is_active': True,
         'date_joined': ('custom', 'date_time_between', {'start_date': '-30d', 'end_date': 'now'}),
-        'password': 'exclude', # password hash slows down seeding
-        'last_login': 'exclude'
+        'password': 'exclude',  # password hash slows down seeding
+        'last_login': 'exclude',
     }
-    default_to = 'faker'
