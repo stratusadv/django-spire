596921c662af7c06879e6582c67d261261b25d2e
O'BrienLittle <obrienl@stratusadv.com>
2025-09-04 19:16:11 -0600

    Fix merge conflicts and x-sort:handle

--- django_spire/knowledge/entry/version/block/querysets.py
+++ django_spire/knowledge/entry/version/block/querysets.py
@@ -1,16 +1,6 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
-from django_spire.contrib.ordering.queryset_mixin import OrderingQuerySetMixin
 from django_spire.contrib.ordering.querysets import OrderingQuerySetMixin
 from django_spire.history.querysets import HistoryQuerySet
 
-if TYPE_CHECKING:
-    from django.db.models import QuerySet
-    from django_spire.knowledge.entry.version.block.models import EntryVersionBlock
-
 
 class EntryVersionBlockQuerySet(HistoryQuerySet, OrderingQuerySetMixin):
-    def greater_or_equal_order(self, order: int) -> QuerySet[EntryVersionBlock]:
-        return self.filter(order__gte=order)
+    pass
