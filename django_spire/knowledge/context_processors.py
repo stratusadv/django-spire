5de498dca2b573bb90bb0c3fad93832b2b81cb1f
Tyrell Martens <167792125+tyrellmstratusadv@users.noreply.github.com>
2025-09-12 18:11:33 -0600

    Knowledge base alpha/base (#120)
    Created side folder navigation for knowledge base.
    
    ---------
    
    Co-authored-by: O'Brien Little <obrienl@stratusadv.com>

---
+++ django_spire/knowledge/context_processors.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from django.core.handlers.wsgi import WSGIRequest
+
+from django_spire.knowledge.collection.models import Collection
+
+
+def django_spire_knowledge(request: WSGIRequest) -> dict[str, Any]:
+    if not request.user.is_authenticated:
+        return {}
+
+    return {
+        'collection_tree_json': Collection.services.transformation.to_hierarchy_json(
+            queryset=Collection.objects.all().select_related('parent').order_by('name'),
+            user=request.user,
+        )
+    }
--- django_spire/knowledge/entry/views/json_views.py
+++ django_spire/knowledge/entry/views/json_views.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 
 from django_spire.core.shortcuts import get_object_or_null_obj
 from django_spire.core.decorators import valid_ajax_request_required
+from django_spire.knowledge.collection.models import Collection
 from django_spire.knowledge.entry.models import Entry
 from django.http import JsonResponse
 
@@ -27,8 +28,22 @@ def reorder_view(request: WSGIRequest) -> JsonResponse:
     if order is None:
         return JsonResponse({'type': 'error', 'message': 'Order not found.'})
 
+    collection_id = body_data.get('collection_id', None)
+    collection = get_object_or_null_obj(Collection, pk=collection_id)
+
+    if collection.id is None:
+        return JsonResponse({'type': 'error', 'message': 'Collection not found.'})
+
+    entry.collection = collection
+    entry.save()
+
     entry.ordering_services.processor.move_to_position(
-        destination_objects=entry.collection.entries.active(),
+        destination_objects=(
+            collection.entries
+            .has_current_version()
+            .user_has_access(user=request.user)
+            .active()
+        ),
         position=order,
     )
 
