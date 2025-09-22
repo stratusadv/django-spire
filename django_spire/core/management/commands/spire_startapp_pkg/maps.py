a7e4a2ae22ce26b62e953b714f5a850cf83a32a8
Ralph Uy <ralphu@stratusadv.com>
2025-09-22 16:22:49 -0600

    Edit permission paths for revers or labels

--- django_spire/core/management/commands/spire_startapp_pkg/maps.py
+++ django_spire/core/management/commands/spire_startapp_pkg/maps.py
@@ -4,7 +4,12 @@ from __future__ import annotations
 def generate_replacement_map(components: list[str]) -> dict[str, str]:
     app_name = components[-1]
     class_name = ''.join(word.title() for word in app_name.split('_'))
-    parent = components[-2] if len(components) > 1 else app_name
+    if len(components)> 1:
+        parents = components[0:-1]
+        parent = components[-2]
+    else:
+        parents = [app_name,]
+        parent = app_name
     module = '.'.join(components)
 
     return {
@@ -14,8 +19,10 @@ def generate_replacement_map(components: list[str]) -> dict[str, str]:
         'SpireChildApps': class_name + 's',
         'spirechildapp': app_name.lower(),
         'spirechildapps': app_name.lower() + 's',
-        'SpireParentApp': class_name,
-        'SpireParentApps': class_name + 's',
         'spireparentapp': parent.lower(),
-        'spireparentapps': parent.lower() + 's'
+        'spireparentapps': parent.lower() + 's',
+        'spireReverseFullParentPath': ':'.join(parents[1:]).lower(),
+        'spireReverseFullChildPath': ':'.join(parents[1:]).lower() + ':' + app_name.lower(),
+        'spireLabelFullChildPath': '_'.join(parents[1:]).lower() + '_' + app_name.lower(),
+        'spireTemplateFullChildPath': '/'.join(parents[1:]).lower() + '/' + app_name.lower(),
     }
