97d7c43e430c88ccc664dba9a0ae39e6549a6b92
Ralph Uy <ralphu@stratusadv.com>
2025-09-23 13:18:44 -0600

    Renamed mapping variables and fix missing slash

--- django_spire/core/management/commands/spire_startapp_pkg/maps.py
+++ django_spire/core/management/commands/spire_startapp_pkg/maps.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 def generate_replacement_map(components: list[str]) -> dict[str, str]:
     app_name = components[-1]
-    class_name = ''.join(word.title() for word in app_name.split('_'))
+    model_name = ''.join(word.title() for word in app_name.split('_'))
     if len(components)> 1:
         parents = components[0:-1]
         parent = components[-2]
@@ -14,15 +14,14 @@ def generate_replacement_map(components: list[str]) -> dict[str, str]:
 
     return {
         'module': module,
-        'spirepermission': parent.lower() + app_name.lower(),
-        'SpireChildApp': class_name,
-        'SpireChildApps': class_name + 's',
-        'spirechildapp': app_name.lower(),
-        'spirechildapps': app_name.lower() + 's',
-        'spireparentapp': parent.lower(),
-        'spireparentapps': parent.lower() + 's',
-        'spireReverseFullParentPath': ':'.join(parents[1:]).lower(),
-        'spireReverseFullChildPath': ':'.join(parents[1:]).lower() + ':' + app_name.lower(),
-        'spireLabelFullChildPath': '_'.join(parents[1:]).lower() + '_' + app_name.lower(),
-        'spireTemplateFullChildPath': '/'.join(parents[1:]).lower() + '/' + app_name.lower(),
+        'spire_model_name': model_name,
+        'spire_model_names': model_name + 's',
+        'spire_app_name': app_name.lower(),
+        'spire_app_names': app_name.lower() + 's',
+        'spire_parent_app': parent.lower(),
+        'spire_parent_apps': parent.lower() + 's',
+        'spire_reverse_parent_path': ':'.join(parents[1:]).lower(),
+        'spire_reverse_path': ':'.join(parents[1:]).lower() + ':' + app_name.lower(),
+        'spire_label_path': '_'.join(parents[1:]).lower() + '_' + app_name.lower(),
+        'spire_template_path': '/'.join(parents[1:]).lower() + '/' + app_name.lower(),
     }
