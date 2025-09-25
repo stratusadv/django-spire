dee98f0c8f38130511aad01b36d62c8d18fc9a85
Ralph Uy <ralphu@stratusadv.com>
2025-09-24 19:41:46 -0600

    Spire Start App: Fine Tune Naming Map
    * Edit permission paths for revers or labels
    
    * Renamed mapping variables and fix missing slash
    
    * Change the fields on AppForm to exclude
    
    * Address code review comments

--- django_spire/core/management/commands/spire_startapp_pkg/maps.py
+++ django_spire/core/management/commands/spire_startapp_pkg/maps.py
@@ -3,19 +3,26 @@ from __future__ import annotations
 
 def generate_replacement_map(components: list[str]) -> dict[str, str]:
     app_name = components[-1]
-    class_name = ''.join(word.title() for word in app_name.split('_'))
-    parent = components[-2] if len(components) > 1 else app_name
+    model_name = ''.join(word.title() for word in app_name.split('_'))
+    if len(components)> 1:
+        parents = components[0:-1]
+        parent = components[-2]
+    else:
+        parents = [app_name,]
+        parent = app_name
     module = '.'.join(components)
 
+    # The sequence is important. Plurals must be replaced first;
+    # otherwise, it will generate _plural variables and names.
     return {
         'module': module,
-        'spirepermission': parent.lower() + app_name.lower(),
-        'SpireChildApp': class_name,
-        'SpireChildApps': class_name + 's',
-        'spirechildapp': app_name.lower(),
-        'spirechildapps': app_name.lower() + 's',
-        'SpireParentApp': class_name,
-        'SpireParentApps': class_name + 's',
-        'spireparentapp': parent.lower(),
-        'spireparentapps': parent.lower() + 's'
+        'spire_model_name_plural': model_name + 's',
+        'spire_model_name': model_name,
+        'spire_app_name_plural': app_name.lower() + 's',
+        'spire_app_name': app_name.lower(),
+        'spire_parent_app': parent.lower(),
+        'spire_reverse_parent_path': ':'.join(parents[1:]).lower(),
+        'spire_reverse_path': ':'.join(parents[1:]).lower() + ':' + app_name.lower(),
+        'spire_label_path': '_'.join(parents[1:]).lower() + '_' + app_name.lower(),
+        'spire_template_path': '/'.join(parents[1:]).lower() + '/' + app_name.lower(),
     }
