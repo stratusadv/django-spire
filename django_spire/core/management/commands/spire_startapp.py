41fb0f5201e1ab012f68d0c17dce59ed7fd9722f
Brayden Carlson <braydenc@stratusadv.com>
2025-09-30 23:03:38 -0600

    Comment out template creation

--- django_spire/core/management/commands/spire_startapp.py
+++ django_spire/core/management/commands/spire_startapp.py
@@ -65,7 +65,7 @@ class Command(BaseCommand):
         if missing:
             self.reporter.report_missing_components(missing)
             self.reporter.report_app_tree_structure(self.app_base, components, registry, self.app_template)
-            self.reporter.report_html_tree_structure(self.template_base, components, registry, self.html_template)
+            # self.reporter.report_html_tree_structure(self.template_base, components, registry, self.html_template)
 
             if not self.reporter.prompt_for_confirmation('\nProceed with app creation? (y/n): '):
                 self.reporter.write('App creation aborted.', self.style.ERROR)
@@ -73,7 +73,7 @@ class Command(BaseCommand):
 
             for module in [missing[-1]]:
                 self.app_manager.create_custom_app(module, self.app_processor, self.reporter)
-                self.html_manager.create_custom_templates(module, self.html_processor, self.reporter)
+                # self.html_manager.create_custom_templates(module, self.html_processor, self.reporter)
 
             self.reporter.report_installed_apps_suggestion(missing)
         else:
