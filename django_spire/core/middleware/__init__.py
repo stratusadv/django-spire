4aa9bf2da8dcb5830cfced6f46e41630e4bc0971
Brayden Carlson <braydenc@stratusadv.com>
2025-10-05 19:12:35 -0600

    Remove old reference

--- django_spire/core/middleware/__init__.py
+++ django_spire/core/middleware/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from django_spire.core.middleware.maintenance import MaintenanceMiddleware
-from django_spire.core.middleware.profiling import ProfilingMiddleware
 
 
-__all__ = ['MaintenanceMiddleware', 'ProfilingMiddleware']
+__all__ = ['MaintenanceMiddleware']
