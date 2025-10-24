9684be361b4d15df7f16546604feae9283b7205e
Tyrell Martens <tyrellm@stratusadv.com>
2025-10-23 19:42:49 -0600

    Import style change.

--- django_spire/knowledge/entry/version/block/data/maps.py
+++ django_spire/knowledge/entry/version/block/data/maps.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 
-import django_spire.knowledge.entry.version.block.data.list.choices
-import django_spire.knowledge.entry.version.block.data.list.data
 from django_spire.knowledge.entry.version.block.choices import BlockTypeChoices
 from django_spire.knowledge.entry.version.block.data.heading_data import \
     HeadingEditorBlockData
 from django_spire.knowledge.entry.version.block.data.text_data import \
     TextEditorBlockData
+from django_spire.knowledge.entry.version.block.data.list.data import \
+    ListEditorBlockData
 
 
 EDITOR_BLOCK_DATA_MAP = {
     BlockTypeChoices.TEXT: TextEditorBlockData,
     BlockTypeChoices.HEADING: HeadingEditorBlockData,
-    BlockTypeChoices.LIST: django_spire.knowledge.entry.version.block.data.list.data.ListEditorBlockData,
+    BlockTypeChoices.LIST: ListEditorBlockData,
 }
 
 
