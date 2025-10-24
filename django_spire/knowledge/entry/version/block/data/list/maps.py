9fd6776347e4bf4bca04c60902054a49d5a83b9c
Tyrell Martens <tyrellm@stratusadv.com>
2025-10-23 19:39:36 -0600

    Removed redundant file and small import style change.

--- django_spire/knowledge/entry/version/block/data/list/maps.py
+++ django_spire/knowledge/entry/version/block/data/list/maps.py
@@ -1,14 +1,21 @@
 from __future__ import annotations
 
-import django_spire.knowledge
+from django_spire.knowledge.entry.version.block.data.list.choices import (
+    ListEditorBlockDataStyle,
+)
+from django_spire.knowledge.entry.version.block.data.list.meta import (
+    ChecklistItemMeta,
+    OrderedListItemMeta,
+)
+
 
 LIST_BLOCK_DATA_META_MAP = {
-    django_spire.knowledge.entry.version.block.data.list.choices.ListEditorBlockDataStyle.ORDERED: django_spire.knowledge.entry.version.block.data.list.meta.OrderedListItemMeta,
-    django_spire.knowledge.entry.version.block.data.list.choices.ListEditorBlockDataStyle.CHECKLIST: django_spire.knowledge.entry.version.block.data.list.meta.ChecklistItemMeta,
-    django_spire.knowledge.entry.version.block.data.list.choices.ListEditorBlockDataStyle.UNORDERED: None,
+    ListEditorBlockDataStyle.ORDERED: OrderedListItemMeta,
+    ListEditorBlockDataStyle.CHECKLIST: ChecklistItemMeta,
+    ListEditorBlockDataStyle.UNORDERED: None,
 }
 LIST_BLOCK_DATA_REVERSE_META_MAP = {
-    django_spire.knowledge.entry.version.block.data.list.meta.OrderedListItemMeta: django_spire.knowledge.entry.version.block.data.list.choices.ListEditorBlockDataStyle.ORDERED,
-    django_spire.knowledge.entry.version.block.data.list.meta.ChecklistItemMeta: django_spire.knowledge.entry.version.block.data.list.choices.ListEditorBlockDataStyle.CHECKLIST,
-    None: django_spire.knowledge.entry.version.block.data.list.choices.ListEditorBlockDataStyle.UNORDERED,
+    OrderedListItemMeta: ListEditorBlockDataStyle.ORDERED,
+    ChecklistItemMeta: ListEditorBlockDataStyle.CHECKLIST,
+    None: ListEditorBlockDataStyle.UNORDERED,
 }
