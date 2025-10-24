d94c55307449ee029bb73de5c400aba3da525a1d
chasemstratusadv <chasem@stratusadv.com>
2025-10-24 09:36:08 -0600

    delete extra files

--- knowledge_import.py
+++
@@ -1,7 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-
-get_wsgi_application()
-
-from django_spire.knowledge.entry.models import Entry
-
-Entry.services.automation.convert_files_to_model_objects()
