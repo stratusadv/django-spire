ffad32ec52c3526466376c98b6a6884993f22345
chasemstratusadv <chasem@stratusadv.com>
2025-10-23 19:09:33 -0600

    reorganize list block code

--- django_spire/knowledge/entry/version/block/data/maps.py
+++ django_spire/knowledge/entry/version/block/data/maps.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from django_spire.knowledge.entry.version.block.data import list_data
+import django_spire.knowledge.entry.version.block.data.list.choices
+import django_spire.knowledge.entry.version.block.data.list.data
 from django_spire.knowledge.entry.version.block.choices import BlockTypeChoices
 from django_spire.knowledge.entry.version.block.data.heading_data import \
     HeadingEditorBlockData
@@ -11,25 +12,11 @@ from django_spire.knowledge.entry.version.block.data.text_data import \
 EDITOR_BLOCK_DATA_MAP = {
     BlockTypeChoices.TEXT: TextEditorBlockData,
     BlockTypeChoices.HEADING: HeadingEditorBlockData,
-    BlockTypeChoices.LIST: list_data.ListEditorBlockData,
+    BlockTypeChoices.LIST: django_spire.knowledge.entry.version.block.data.list.data.ListEditorBlockData,
 }
 
 
 EDITOR_BLOCK_DATA_REVERSE_MAP = {
     TextEditorBlockData: BlockTypeChoices.TEXT,
     HeadingEditorBlockData: BlockTypeChoices.HEADING,
-}
-
-
-LIST_BLOCK_DATA_META_MAP = {
-    list_data.ListEditorBlockDataStyle.ORDERED: list_data.OrderedListItemMeta,
-    list_data.ListEditorBlockDataStyle.CHECKLIST: list_data.ChecklistItemMeta,
-    list_data.ListEditorBlockDataStyle.UNORDERED: None,
-}
-
-
-LIST_BLOCK_DATA_REVERSE_META_MAP = {
-    list_data.OrderedListItemMeta: list_data.ListEditorBlockDataStyle.ORDERED,
-    list_data.ChecklistItemMeta: list_data.ListEditorBlockDataStyle.CHECKLIST,
-    None: list_data.ListEditorBlockDataStyle.UNORDERED,
 }
