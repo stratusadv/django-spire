426d2954c8f8aa42cefa1467ae02cf2d490b734c
Nathan Johnson <nathanj@stratusadv.com>
2025-11-08 22:23:33 -0700

    working on testing

--- django_spire/core/tags/tests/test_tags.py
+++ django_spire/core/tags/tests/test_tags.py
@@ -1,7 +1,5 @@
-from django.db import models
 from django.test import TestCase
 
-from django_spire.core.tags.mixins import TagsModelMixin
 from django_spire.core.tags.models import Tag
 from django_spire.knowledge.collection.models import Collection
 
@@ -13,18 +11,26 @@ class TestTags(TestCase):
             description='Test Collection Description'
         )
 
-        self.new_tags = {'apple', 'banana', 'orange'}
-        self.messy_tags = {'HaT', 'four tacos', '123 hoop'}
+        self.tag_set_a = {'apple', 'banana', 'orange'}
+        self.tag_set_b = {'grape', 'kiwi', 'pear'}
+        self.tag_set_c = {'lemon', 'lime', 'mango'}
+        self.mixed_tag_set = {'apple', 'grape', 'lemon'}
+        self.bad_tag_set = {'HaT~', 'four* tacos', '123 hoop!!'}
 
     def test_tag_model(self):
-        self.collection.add_tag_set(self.new_tags)
+        self.collection.add_tag_set(self.tag_set_a)
+        self.collection.add_tag_set(self.tag_set_b)
+        self.collection.add_tag_set(self.tag_set_c)
+        self.collection.add_tag_set(self.mixed_tag_set)
+        self.collection.add_tag_set(self.bad_tag_set)
 
-        self.assertEqual(Tag.objects.count(), 3)
+        print(Tag.objects.all())
+        self.assertEqual(Tag.objects.count(), 12)
+        print(self.collection.tags.all())
+        self.assertEqual(self.collection.tags.count(), 12)
+        self.assertEqual(self.collection.aggregated_tags.count(), 12)
 
-        self.collection.add_tag_set(self.messy_tags)
+        self.collection.remove_tag_set(self.tag_set_a)
 
-        self.assertEqual(Tag.objects.count(), 6)
+        self.assertEqual(Tag.objects.count(), 9)
 
-        self.collection.add_tag_set(self.new_tags)
-
-        self.assertEqual(Tag.objects.count(), 6)
