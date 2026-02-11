6f447f44d68c7850d7f43942b76e49385ae9e5fa
O'BrienLittle <obrienl@stratusadv.com>
2026-02-11 16:20:43 -0700

    Fixed tests

--- django_spire/contrib/responses/json_response.py
+++ django_spire/contrib/responses/json_response.py
@@ -11,19 +11,28 @@ def info_json_response(message: str | None = None, **kwargs) -> JsonResponse:
     return json_response(type=ResponseTypeChoices.INFO, message=message, **kwargs)
 
 
-def json_response(type: ResponseTypeChoices, message: str | None = None, **kwargs) -> JsonResponse:
-    response_choices = [choice.value for choice in ResponseTypeChoices]
-    if type.value not in response_choices:
+def json_response(type: ResponseTypeChoices | str, message: str | None = None, **kwargs) -> JsonResponse:
+    # Validate and normalize the type parameter
+    if isinstance(type, ResponseTypeChoices):
+        type_value = type.value
+    elif isinstance(type, str):
+        response_choices = [choice.value for choice in ResponseTypeChoices]
+        if type not in response_choices:
+            valid = ', '.join(response_choices)
+            raise ValueError(f'{type} is not a valid option for ResponseTypeChoices: [{valid}]')
+        type_value = type
+    else:
+        response_choices = [choice.value for choice in ResponseTypeChoices]
         valid = ', '.join(response_choices)
         raise ValueError(f'{type} is not a valid option for ResponseTypeChoices: [{valid}]')
 
     return_data = {
-        'type': type.value,
+        'type': type_value,
         **kwargs,
     }
 
     if message is not None:
-        return_data.update({'message': message})
+        return_data['message'] = message
 
     return JsonResponse(return_data)
 
