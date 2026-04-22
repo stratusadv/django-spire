db4ccb587af76558959abad031343c174a701259
Tyrell Martens <tyrellm@stratusadv.com>
2026-04-22 08:17:32 -0600

    Implemented access key decorator.

--- django_spire/core/decorators.py
+++ django_spire/core/decorators.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 from functools import wraps
+from typing import TYPE_CHECKING, Callable, ParamSpec, TypeVar
 
-from typing import Callable, ParamSpec, TypeVar
-
+from django.conf import settings
 from django.db import connections
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import Http404, HttpRequest, HttpResponse, JsonResponse
+
+if TYPE_CHECKING:
+    from django.core.handlers.wsgi import WSGIRequest
 
 
 P = ParamSpec('P')
@@ -21,6 +24,27 @@ def close_db_connections(func: Callable[P, T]) -> Callable[P, T]:
             connections.close_all()
 
     return inner
+
+
+def require_access_key(
+    setting_name: str,
+    param_name: str = 'access_key',
+) -> Callable[..., Callable[..., HttpResponse]]:
+    def decorator(view: Callable[..., HttpResponse]) -> Callable[..., HttpResponse]:
+        @wraps(view)
+        def wrapper(request: WSGIRequest, *args, **kwargs) -> HttpResponse:
+            key = getattr(settings, setting_name, None)
+            if not key:
+                raise Http404
+
+            if str(request.GET.get(param_name)) != str(key):
+                raise Http404
+
+            return view(request, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
 
 
 def valid_ajax_request_required(
