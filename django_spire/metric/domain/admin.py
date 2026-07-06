2d95999df430d6f55f844366f2fc2f26f8f1426e
Brian Park <brianp@stratusadv.com>
2026-07-06 10:58:50 -0600

    adding subdomain admin panel

--- django_spire/metric/domain/admin.py
+++ django_spire/metric/domain/admin.py
@@ -2,12 +2,20 @@ from __future__ import annotations
 
 from django.contrib import admin
 
-from django_spire.metric.domain.models import Domain
+from django_spire.metric.domain.models import Domain, SubDomain
 
 
 @admin.register(Domain)
 class DomainAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'description', 'sub_domain_description', 'created_datetime')
+    list_filter = ('is_active', 'is_deleted')
+    ordering = ('-created_datetime',)
+    readonly_fields = ('created_datetime', 'is_active', 'is_deleted')
+    search_fields = ('description', 'created_by__username', 'created_by__email')
+
+@admin.register(SubDomain)
+class SubDomainAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description', 'created_datetime')
     list_filter = ('is_active', 'is_deleted')
     ordering = ('-created_datetime',)
     readonly_fields = ('created_datetime', 'is_active', 'is_deleted')
