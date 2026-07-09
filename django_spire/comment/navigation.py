553cbb843c43f1c139a6c0db54a59c7c9eb7d1ab
Brian Park <brianp@stratusadv.com>
2026-07-09 09:15:10 -0600

    fixing broken links & adding breadcrums for the comment app

--- django_spire/comment/navigation.py
+++ django_spire/comment/navigation.py
@@ -5,5 +5,5 @@ class CommentNavigation(Navigation):
     def __init__(self) -> None:
         super().__init__()
         self.icon_class = 'bi bi-chat'
-        self.breadcrumbs.add(name='Comments', view_name='django_spire:comment:page:list')
+        self.breadcrumbs.add(name='Comments', view_name='comment:page:list')
         self.page_title = 'Comments'
