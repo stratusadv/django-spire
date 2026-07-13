1d3ff951f164627a343a419f9a51fc371ad28c0b
Nathan Johnson <nathanj@stratusadv.com>
2026-07-13 10:14:24 -0600

    task clean up

--- test_project/app/task/views/form_views.py
+++ test_project/app/task/views/form_views.py
@@ -8,7 +8,6 @@ from django.urls import reverse
 
 from django_glue import Glue
 
-from django_spire.contrib.form.tools import show_form_errors
 from django_spire.contrib.redirects import safe_redirect_url
 from django_spire.contrib.shortcuts import get_object_or_null_obj
 
@@ -19,59 +18,22 @@ if TYPE_CHECKING:
     from django.core.handlers.wsgi import WSGIRequest
 
 
-def glue_form_view(request: WSGIRequest, pk: int) -> TemplateResponse | redirect:
+def form_view(request: WSGIRequest, pk: int) -> TemplateResponse:
     task = get_object_or_null_obj(models.Task, pk=pk)
 
     nav = TaskNavigation()
     nav.set_page_title_from_model_instance_form_action(task)
     nav.breadcrumbs.add(f'{task.name}' if task.pk else 'New Task (With Glue)')
 
-    form = forms.TaskGlueModelForm(request.POST or None, instance=task)
+    form = forms.TaskModelForm(request.POST or None, instance=task)
 
     Glue.form(request, 'task_model_form', form, Glue.Access.DELETE)
 
     context = {**nav.as_context()}
 
     return TemplateResponse(
-        request=request, context=context, template='task/page/glue_form_page.html'
+        request=request, context=context, template='task/page/form_page.html'
     )
-
-
-def create_view(request: WSGIRequest) -> TemplateResponse | redirect:
-    task = models.Task()
-
-    return _form_view(request, task)
-
-
-def update_view(request: WSGIRequest, pk: int) -> TemplateResponse | redirect:
-    task = get_object_or_404(models.Task, pk=pk)
-
-    return _form_view(request, task)
-
-
-def _form_view(request: WSGIRequest, task: models.Task) -> TemplateResponse | redirect:
-    if request.method == 'POST':
-        form = forms.TaskModelForm(request.POST, instance=task)
-
-        if form.is_valid():
-            task.services.save_model_obj(user=request.user, obj=task, **form.cleaned_data)
-
-            return redirect(request.GET.get('return_url', reverse('task:page:list')))
-
-        show_form_errors(request, form)
-    else:
-        form = forms.TaskModelForm(instance=task)
-
-    nav = TaskNavigation()
-    nav.set_page_title_from_model_instance_form_action(task)
-    nav.breadcrumbs.add(f'{task.name}' if task.pk else 'New Task')
-
-    context = nav.as_context()
-    context['form'] = form
-    context['task'] = task
-
-    return TemplateResponse(request=request, context=context, template='task/page/form_page.html')
-
 
 def delete_view(request: WSGIRequest, pk: int) -> TemplateResponse | redirect:
     task = get_object_or_404(models.Task, pk=pk)
