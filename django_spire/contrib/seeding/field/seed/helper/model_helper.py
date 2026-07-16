624fc63c797be58cc460e99d51711305e5ca265c
Nathan Johnson <nathanj@stratusadv.com>
2026-07-16 12:21:00 -0600

    seeding

--- django_spire/contrib/seeding/field/seed/helper/model_helper.py
+++ django_spire/contrib/seeding/field/seed/helper/model_helper.py
@@ -1,17 +1,20 @@
-from typing import Any
+import random
 
 from django.db import models
 from django.db.models import Choices
 
+from django_spire.contrib.seeding.field.seed.callable_seed import CallableFieldSeed
 from django_spire.contrib.seeding.field.seed.helper.helper import FieldSeedHelper
-from django_spire.contrib.seeding.field.seed.model_seed import ModelFieldSeed
 from django_spire.contrib.seeding.field.seed.random_seed import RandomFieldSeed
 
 
 class ModelFieldSeedHelper(FieldSeedHelper):
     @staticmethod
-    def random_foreign_key(model_class: type[models.Model]) -> ModelFieldSeed:
-        return ModelFieldSeed(model_class=model_class)
+    def random_foreign_key(model_class: type[models.Model]) -> CallableFieldSeed:
+        return CallableFieldSeed(
+            random.choice,
+            seq=list(model_class.objects.all().values_list('id', flat=True))
+        )
 
     @staticmethod
     def random_field_choice(choices: Choices) -> RandomFieldSeed:
