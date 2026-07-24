bc837d13717a7390d176671a968d527741041cc9
chasemstratusadv <chasem@stratusadv.com>
2026-07-23 21:12:16 -0600

    fix uvlock

--- test_project/app/task/seeding/seed.py
+++ test_project/app/task/seeding/seed.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from test_project.app.task.seeding.seeder import (
     SubTaskModelSeeder,
+    SubSubTaskModelSeeder,
     TaskModelSeeder,
     TaskUserModelSeeder,
 )
@@ -15,6 +16,11 @@ task_model_seeder.seed_database()
 sub_task_model_seeder = SubTaskModelSeeder(count=100)
 
 sub_task_model_seeder.seed_database()
+
+
+sub_sub_task_model_seeder = SubSubTaskModelSeeder(count=100)
+
+sub_sub_task_model_seeder.seed_database()
 
 
 task_user_model_seeder = TaskUserModelSeeder(count=400)
