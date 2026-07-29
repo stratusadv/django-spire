d9c4a758c687fc70823f51ed333ae6f60cb89a19
Nathan Johnson <nathanj@stratusadv.com>
2026-07-29 13:36:04 -0600

    file seeding

--- django_spire/contrib/seeding/field/seed/file_seed.py
+++ django_spire/contrib/seeding/field/seed/file_seed.py
@@ -9,16 +9,23 @@ from django_spire.contrib.seeding.field.seed.base import BaseFieldSeed
 
 
 class FileFieldSeed(BaseFieldSeed):
-    def __init__(self) -> None:
-        self._db_value = '.seeding/seeded_file.txt'
+    def __init__(self, upload_to: str | None = None) -> None:
+        if upload_to:
+            self._seeding_dir = Path(upload_to, '.seeding')
+        else:
+            self._seeding_dir = '.seeding'
+
+        self._seeding_txt_file = 'seeded_file.txt'
+
+        self._seeding_file_path = Path(self._seeding_dir, self._seeding_txt_file)
 
     def generate_value(self, seed_index: int) -> Any:
         if seed_index == -1:
-            seeder_dir = Path(settings.MEDIA_ROOT) / '.seeding'
+            seeder_dir = Path(settings.MEDIA_ROOT) / self._seeding_dir
             seeder_dir.mkdir(parents=True, exist_ok=True)
 
             file_path = seeder_dir / 'seeded_file.txt'
             if not file_path.exists():
                 file_path.write_text('Hello World')
 
-        return self._db_value
+        return self._seeding_file_path
