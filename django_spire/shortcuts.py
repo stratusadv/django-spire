d792e5fc86a9943f7842500919586a277d5a4e36
Nathan Johnson <nathanj@stratusadv.com>
2026-07-31 11:12:57 -0600

    url configuration

---
+++ django_spire/shortcuts.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from django_spire.constants import BASE_URL_NAME
+
+
+def django_spire_urls() -> list:
+    return [
+        path(
+            f'{BASE_URL_NAME}/',
+            include('django_glue.urls', namespace=BASE_URL_NAME),
+        )
+    ]
